2924264c41a1f929f106afb512c72a100657ac6a
Kelvin <duchung97dl@gmail.com>
2021-07-28 21:35:36 +1000

    Create dedup function.

---
+++ dedup.mjs
@@ -0,0 +1,23 @@
+/**
+ * Creates a duplicate-free version of an array for equality comparisons,
+ * in which only the first occurrence of each element is kept.
+ * The order of result values is determined by the order they occur
+ * in the array.
+ *
+ * @param {Array} array The array to inspect.
+ * @returns {Array} Returns the new duplicate free array.
+ * @example
+ *
+ * dedup([2, 1, 2])
+ * // => [2, 1]
+ */
+
+function dedup(array) {
+  if (array == null && array.length === 0) {
+    return [];
+  }
+
+  return Array.from(new Set(array));
+}
+
+export default dedup;
